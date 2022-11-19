8c4cc77efa1045bb9231159e447e6f5efe550338
M_Ulaonovych <m.ulanovych@gmail.com>
2022-11-19 15:39:36 +0200

    feat: write function for modal, they close all modals if user click in pad

--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -3,23 +3,37 @@ const modals = () => {
 		const trigger = document.querySelectorAll(triggerSelector);
 		const modal = document.querySelector(modalSelector);
 		const close = document.querySelector(closeSelector);
+		const windows = document.querySelectorAll('[data-modal]');
 
 		trigger.forEach(item => {
 			item.addEventListener('click', e => {
 				if (e.target) {
 					e.preventDefault();
 				}
+
+				windows.forEach(item => {
+					item.style.display = 'none';
+				});
+
 				modal.style.display = 'block';
 				document.body.classList.add('modal-open');
 			});
 		});
 
 		close.addEventListener('click', () => {
+			windows.forEach(item => {
+				item.style.display = 'none';
+			});
+
 			modal.style.display = 'none';
 			document.body.classList.remove('modal-open');
 		});
 
 		modal.addEventListener('click', e => {
+			windows.forEach(item => {
+				item.style.display = 'none';
+			});
+
 			if (e.target === modal) {
 				modal.style.display = 'none';
 				document.body.classList.remove('modal-open');
